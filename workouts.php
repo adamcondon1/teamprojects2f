0a492c2ff3d037b78bbd4d02f624475ae08b3a49
ggjambo <33702567+ggjambo@users.noreply.github.com>
2020-12-15 19:17:12 +0000

    Added extra categories to workouts

--- workouts.php
+++ workouts.php
@@ -15,18 +15,127 @@
 	<div class="wrapper">
 		<div class="bodyWrapper">
 			<div class="bodyWrapper1">
-				<h1 class="title">Workouts</h1>
+				<h2 id="titleWorkout">Beginner: Lower Body</h2>
 				<div id="container">
 					<?php
 						require_once 'includes/dbh.inc.php';
-						$sql = "SELECT * FROM workoutsetsreps";
+						$sql = "SELECT * FROM workoutsetsreps WHERE workoutLevel= 'Beginner' AND workoutBodySection='Lower'";
 						$result = $conn->query($sql);
 						
 						if ($result->num_rows > 0) {
 							while($row = $result->fetch_assoc()) {
 								echo "<div class='workout'>";
-									echo $row['workoutLevel'] . "<br><br>";
-									echo "Workout Name: " . $row['workoutName'] . "<br>";
+									echo "Name: " . $row['workoutName'] . "<br>";
+									echo "Sets Total - " . $row['workoutSets']. "<br>" . "Reps Total - " . $row['workoutReps'];
+									echo "<br>" . "Workout Description: " . $row["workoutDesc"] . "<br>";	
+								echo "</div>";
+							}
+						}
+						else{
+							echo "0 results";
+						}
+					?>
+				</div>
+				<br></br>
+				<h2 id="titleWorkout">Beginner: Upper Body</h2>
+				<div id="container">
+					<?php
+						require_once 'includes/dbh.inc.php';
+						$sql = "SELECT * FROM workoutsetsreps WHERE workoutLevel= 'Beginner' AND workoutBodySection='Upper'";
+						$result = $conn->query($sql);
+						
+						if ($result->num_rows > 0) {
+							while($row = $result->fetch_assoc()) {
+								echo "<div class='workout'>";
+									echo "Name: " . $row['workoutName'] . "<br>";
+									echo "Sets Total - " . $row['workoutSets']. "<br>" . "Reps Total - " . $row['workoutReps'];
+									echo "<br>" . "Workout Description: " . $row["workoutDesc"] . "<br>";	
+								echo "</div>";
+							}
+						}
+						else{
+							echo "0 results";
+						}
+					?>
+				</div>
+				<br></br>
+				<h2 id="titleWorkout">Intermediate: Lower Body</h2>
+				<div id="container">
+					<?php
+						require_once 'includes/dbh.inc.php';
+						$sql = "SELECT * FROM workoutsetsreps WHERE workoutLevel= 'Intermediate' AND workoutBodySection='Lower'";
+						$result = $conn->query($sql);
+						
+						if ($result->num_rows > 0) {
+							while($row = $result->fetch_assoc()) {
+								echo "<div class='workout'>";
+									echo "Name: " . $row['workoutName'] . "<br>";
+									echo "Sets Total - " . $row['workoutSets']. "<br>" . "Reps Total - " . $row['workoutReps'];
+									echo "<br>" . "Workout Description: " . $row["workoutDesc"] . "<br>";	
+								echo "</div>";
+							}
+						}
+						else{
+							echo "0 results";
+						}
+					?>
+				</div>
+				<br></br>
+				<h2 id="titleWorkout">Intermediate: Upper Body</h2>
+				<div id="container">
+					<?php
+						require_once 'includes/dbh.inc.php';
+						$sql = "SELECT * FROM workoutsetsreps WHERE workoutLevel= 'Intermediate' AND workoutBodySection='Upper'";
+						$result = $conn->query($sql);
+						
+						if ($result->num_rows > 0) {
+							while($row = $result->fetch_assoc()) {
+								echo "<div class='workout'>";
+									echo "Name: " . $row['workoutName'] . "<br>";
+									echo "Sets Total - " . $row['workoutSets']. "<br>" . "Reps Total - " . $row['workoutReps'];
+									echo "<br>" . "Workout Description: " . $row["workoutDesc"] . "<br>";	
+								echo "</div>";
+							}
+						}
+						else{
+							echo "0 results";
+						}
+					?>
+				</div>
+				<br></br>
+				<h2 id="titleWorkout">Professional: Lower Body</h2>
+				<div id="container">
+					<?php
+						require_once 'includes/dbh.inc.php';
+						$sql = "SELECT * FROM workoutsetsreps WHERE workoutLevel= 'Professional' AND workoutBodySection='Lower'";
+						$result = $conn->query($sql);
+						
+						if ($result->num_rows > 0) {
+							while($row = $result->fetch_assoc()) {
+								echo "<div class='workout'>";
+									echo "Name: " . $row['workoutName'] . "<br>";
+									echo "Sets Total - " . $row['workoutSets']. "<br>" . "Reps Total - " . $row['workoutReps'];
+									echo "<br>" . "Workout Description: " . $row["workoutDesc"] . "<br>";	
+								echo "</div>";
+							}
+						}
+						else{
+							echo "0 results";
+						}
+					?>
+				</div>
+				<br></br>
+				<h2 id="titleWorkout">Professional: Upper Body</h2>
+				<div id="container">
+					<?php
+						require_once 'includes/dbh.inc.php';
+						$sql = "SELECT * FROM workoutsetsreps WHERE workoutLevel= 'Professional' AND workoutBodySection='Upper'";
+						$result = $conn->query($sql);
+						
+						if ($result->num_rows > 0) {
+							while($row = $result->fetch_assoc()) {
+								echo "<div class='workout'>";
+									echo "Name: " . $row['workoutName'] . "<br>";
 									echo "Sets Total - " . $row['workoutSets']. "<br>" . "Reps Total - " . $row['workoutReps'];
 									echo "<br>" . "Workout Description: " . $row["workoutDesc"] . "<br>";	
 								echo "</div>";
@@ -42,7 +151,6 @@
 			
 			</div>
 			<div class="bodyWrapper3">
-			
 			</div>
 		</div>
 	</div>
